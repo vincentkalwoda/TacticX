c78bb2b45b5436ba6b346a86d1b1ebe3d5c043e2
Vincent Kalwoda <kal210629@spengergasse.at>
2023-12-19 16:05:34 +0100

    Verein hinzugefügt

---
+++ js/verein.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+    $('.dashboard').on('click', function () {
+        postData('/getDashboard')
+            .then(response => {
+                // Handle the response, e.g., check for success and redirect if needed
+                if (response.ok) {
+                    window.location.href = '/dashboard';
+                } else {
+                    console.error('Error:', response.statusText);
+                    // Handle the error, e.g., show an error message to the user
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle the error, e.g., show an error message to the user
+            });
+    });
+
+    $('.liga').on('click', function () {
+        postData('/getLiga')
+            .then(response => {
+                // Handle the response, e.g., check for success and redirect if needed
+                if (response.ok) {
+                    window.location.href = '/verein';
+                } else {
+                    console.error('Error:', response.statusText);
+                    // Handle the error, e.g., show an error message to the user
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle the error, e.g., show an error message to the user
+            });
+    });
+
+    $('.verein').on('click', function () {
+        location.reload();
+    });
+    $('.marktplatz').on('click', function () {
+        postData('/getMarktplatz')
+            .then(response => {
+                // Handle the response, e.g., check for success and redirect if needed
+                if (response.ok) {
+                    window.location.href = '/marktplatz';
+                } else {
+                    console.error('Error:', response.statusText);
+                    // Handle the error, e.g., show an error message to the user
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle the error, e.g., show an error message to the user
+            });
+    });
+
+    function postData(url) {
+        // Use fetch API to perform a POST request
+        return fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+    }
+});
